6735a0d38a733d7a27e54b117e8b61b4d17a831b
Ryan Ackermann <ryan@ackermann.io>
2023-12-01 14:53:20 -0800

    updated the day one solution to use the new template

--- day1.rb
+++ day1.rb
@@ -10,56 +10,74 @@ class Solver
     @year = year
   end
 
-  def decipher
-    fetch_input
-    first_answer = solve_first(@input)
-    puts "the first answer is: #{first_answer}"
-    second_answer = solve_second(@input)
-    puts "the second answer is: #{second_answer}"
-  end
-
   def fetch_input
-    filename = 'cookie.txt'
-    unless File.exist?(filename)
+    input_directory = 'input'
+    unless Dir.exist? input_directory
+      Dir.mkdir input_directory
+    end
+    
+    input_file_name = "#{@year}-#{@day}-input.txt"
+    input_file_path = File.join input_directory, input_file_name
+    
+    if File.exist? input_file_path
+      input_file = File.open input_file_path
+      @input = input_file.read.chomp
+      input_file.close
+      return
+    end
+    
+    auth_filename = 'cookie.txt'
+    unless File.exist? auth_filename
       abort('"cookie.txt" is required to get the puzzle input for your account.')
     end
 
-    file = File.open(filename)
+    cookie_file = File.open auth_filename
 
     uri = URI("https://adventofcode.com/#{@year}/day/#{@day}/input")
-    cookie_value = file.read.chomp
-    user_agent = 'github.com/naturaln0va/aoc2022 by Ryan Ackermann'
+    cookie_value = cookie_file.read.chomp
+    user_agent = 'github.com/naturaln0va/aoc2023 by Ryan Ackermann'
     headers = { 'Cookie' => "session=#{cookie_value}", 'User-Agent' => user_agent }
     
-    file.close
+    cookie_file.close
 
     puts "fetching the puzzle input for day #{@day}..."
     res = Net::HTTP.get_response(uri, headers)
 
-    unless res.is_a?(Net::HTTPSuccess)
+    unless res.is_a? Net::HTTPSuccess
       abort(res.body)
     end
     
     @input = res.body
+    
+    File.write(input_file_path, @input)
   end
 
   def test_case
-    test_input = "1abc2
+    puts "===TEST==="
+    first_test_input = "1abc2
     pqr3stu8vwx
     a1b2c3d4e5f
     treb7uchet"
-    first_answer = solve_first(test_input) # 142
-    puts "the first test answer is: #{first_answer}"
+    first_answer = solve_first(first_test_input)
+    puts "1st answer: #{first_answer}"
     second_test_input = "two1nine
     eightwothree
     abcone2threexyz
     xtwone3four
     4nineeightseven2
     zoneight234
-    7pqrstsixteen
-    eighthree5sevenine"
+    7pqrstsixteen"
     second_answer = solve_second(second_test_input)
-    puts "the second test answer is: #{second_answer}"
+    puts "2nd answer: #{second_answer}"    
+  end
+
+  def decipher
+    fetch_input
+    puts "===PUZZLE==="
+    first_answer = solve_first(@input)
+    puts "1st answer: #{first_answer}"
+    second_answer = solve_second(@input)
+    puts "2nd answer: #{second_answer}"
   end
 
   def solve_first(input)
@@ -70,21 +88,14 @@ class Solver
     end
     numbers.sum
   end
-
+  
   def solve_second(input)
     lines = input.lines.map(&:strip)
     numbers = []
     for line in lines do
       nums = get_nums_in(line)
-      puts nums.join
-      # if nums.length == 1
-      #   numbers << nums.first
-      # else
-      #   numbers << [nums.first, nums.last].map { |n| n.to_s }.join.to_i 
-      # end
       numbers << [nums.first, nums.last].map { |n| n.to_s }.join.to_i 
     end
-    # puts numbers
     numbers.sum
   end
   
@@ -116,5 +127,7 @@ class Solver
 end
 
 s = Solver.new(day = 1)
-s.decipher
 s.test_case
+puts ""
+s.decipher
+

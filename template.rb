4fb5eb470a6c29fd56c64697eb58ecb05785dafc
Ryan Ackermann <ryan@ackermann.io>
2023-12-01 08:16:31 -0800

    initial commit

---
+++ template.rb
@@ -0,0 +1,71 @@
+# This script was written for the advent of code 2023 day X.
+# https://adventofcode.com/2023/day/X
+
+require 'uri'
+require 'net/http'
+
+class Solver
+  def initialize(day, year = 2023)
+    @day = day
+    @year = year
+  end
+
+  def decipher
+    fetch_input
+    first_answer = solve_first(@input)
+    puts "the first answer is: #{first_answer}"
+    second_answer = solve_second(@input)
+    puts "the second answer is: #{second_answer}"
+  end
+
+  def fetch_input
+    filename = 'cookie.txt'
+    unless File.exist?(filename)
+      abort('"cookie.txt" is required to get the puzzle input for your account.')
+    end
+
+    file = File.open(filename)
+
+    uri = URI("https://adventofcode.com/#{@year}/day/#{@day}/input")
+    cookie_value = file.read.chomp
+    user_agent = 'github.com/naturaln0va/aoc2022 by Ryan Ackermann'
+    headers = { 'Cookie' => "session=#{cookie_value}", 'User-Agent' => user_agent }
+    
+    file.close
+
+    puts "fetching the puzzle input for day #{@day}..."
+    res = Net::HTTP.get_response(uri, headers)
+
+    unless res.is_a?(Net::HTTPSuccess)
+      abort(res.body)
+    end
+    
+    @input = res.body
+  end
+
+  def test_case
+    test_input = ""
+    first_answer = solve_first(test_input)
+    puts "the first test answer is: #{first_answer}"
+    second_answer = solve_second(test_input)
+    puts "the second test answer is: #{second_answer}"
+  end
+
+  def solve_first(input)
+    answer = 0
+    lines = input.lines.map(&:strip)
+    numbers = input.split().map { |s| s.to_i }
+    answer
+  end
+
+  def solve_second(input)
+    answer = 0
+    lines = input.lines.map(&:strip)
+    numbers = input.split().map { |s| s.to_i }
+    answer
+  end
+end
+
+s = Solver.new(day = 1)
+# s.decipher
+s.test_case
